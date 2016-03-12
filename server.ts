d1db9e1182cebe880b992596522595b3a5d87337
Brad Smith <bradmsmith@live.com>
2016-03-11 23:29:55 -0800

    header boilerplat in bootstrap

--- server.ts
+++ server.ts
@@ -31,6 +31,7 @@ function renderFullHtmlPage(render: () => Promise<React.ReactElement<any>>): Pro
           <head>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <script src="/ui/client.bundle.js" async></script>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
           </head>
           <body>
             <div id="root">
--- ui/components/header.ts
+++ ui/components/header.ts
@@ -1,84 +1,103 @@
 import * as React from 'react';
-import * as bs from "../bootstrap";
-import * as md from "../material";
 import logo from "./logo";
-import {LeftNav} from 'material-ui';
+
+import {Grid, Row, Col, MenuItem, Nav, Navbar, NavDropdown, NavItem} from 'react-bootstrap';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-const {img, ul, li, nav, button} = React.DOM;
+const {a} = React.DOM;
 
-function leftNav(props, ...children) {
-  return React.createElement(LeftNav, props, ...children);
+//TODO: long list of functions
+function grid(props, ...children) {
+  return React.createElement(Grid, props, ...children);
 }
 
-const styles = {
-  leftNav(isOpen: boolean) {
-    return {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      transition: 'transform 200ms ease-in-out',
-      transform: isOpen ? 'translateX(0)': 'translateX(-100%)',
-      zIndex: 1,
-      backgroundColor: 'white',
-    };
-  },
-};
+function row(props, ...children) {
+  return React.createElement(Row, props, ...children);
+}
 
-class Header extends React.Component<void,{isLeftNavOpen:boolean}> {
-  constructor(props) {
-    super(props);
+function col(props, ...children) {
+  return React.createElement(Col, props, ...children);
+}
 
-    this.state = {isLeftNavOpen: false};
-  }
+function menuitem(props, ...children) {
+  return React.createElement(MenuItem, props, ...children);
+}
 
-  toggleNav = (e) => {
-    const willBeOpen = !this.state.isLeftNavOpen;
-    this.setState({isLeftNavOpen: willBeOpen});
-  }
+function nav(props, ...children) {
+  return React.createElement(Nav, props, ...children);
+}
 
+function navbar(props, ...children) {
+  return React.createElement(Navbar, props, ...children);
+}
+
+function navbarbrand(props, ...children) {
+  return React.createElement(Navbar.Brand, props, ...children);
+}
+
+function navbarcollapse(props, ...children) {
+  return React.createElement(Navbar.Collapse, props, ...children);
+}
+
+function navbarheader(props, ...children) {
+  return React.createElement(Navbar.Header, props, ...children);
+}
+
+function navbartoggle(props, ...children) {
+  return React.createElement(Navbar.Toggle, props, ...children);
+}
+
+function navitem(props, ...children) {
+  return React.createElement(NavItem, props, ...children);
+}
+
+function navdropdown(props, ...children) {
+  return React.createElement(NavDropdown, props, ...children);
+}
+
+class Header extends React.Component<void, void> {
   render() {
     return React.DOM.header({},
-      button({onClick: this.toggleNav},
-        img({src: md.icons.svg.menu.black.src, height: 24, width: 24, alt: "Menu"})
-      ),
 
-      logo(),
+      grid({},
 
-      leftNav({open: this.state.isLeftNavOpen, docked: false},
-        bs.nav({onClick: this.toggleNav}, [
-          bs.navDropdown({ key: 'who-we-are', title: 'Who we are', id: 'header-who-we-are'}, [
-            bs.navItem({ key: 'who-we-are', href: '/who-we-are' }, 'Who we are'),
-            bs.navItem({ key: 'beliefs', href: '/what-we-believe' }, 'What we believe'),
-            bs.navItem({ key: 'distinctives', href: '/eight-distinctives' }, '8 distinctives'),
-          ]),
-          bs.navDropdown({ key: 'ministries', title: 'Ministries', id: 'header-ministries' }, [
-            bs.navItem({ key: 'kids', href: '/kids' }, 'Kids Ministry'),
-            bs.navItem({ key: 'youth', href: '/youth' }, 'Youth Ministry'),
-            bs.navItem({ key: 'college', href: '/college' }, 'College Ministry'),
-            bs.navItem({ key: 'sundayschool', href: '/sundayschool' }, 'Adult Sunday School'),
-            bs.navItem({ key: 'fellowship', href: '/fellowship' }, 'Home Fellowship Groups'),
-            bs.navItem({ key: 'men', href: '/men' }, 'Mens Ministry'),
-            bs.navItem({ key: 'women', href: '/women' }, 'Womens Ministry'),
-          ]),
-          bs.navDropdown({ key: 'sermons', title: 'Sermons', id: 'header-sermons' }, [
-            bs.navItem({ key: 'sermons', href: '/sermons' }, 'Sermons'),
-            bs.navItem({ key: 'read', href: '/read' }, 'Scripture of the Day'),
-            bs.navItem({ key: 'blog', href: '/blog' }, 'Blog'),
-            bs.navItem({ key: 'songs', href: '/songs' }, 'Songs'),
-            bs.navItem({ key: 'events', href: '/events' }, 'Events'),
-            bs.navItem({ key: 'give', href: '/give' }, 'Give Online'),
-          ]),
-          bs.navItem({ key: 'ice-cream-evangelism', href: '/ice-cream-evangelism' }, 'Ice Cream Evangelism'),
-          bs.navItem({ key: 'search', href: '/search' }, 'Search'),
-        ])
+        row({style: {borderTop: '5px solid #497f9e'}},
+          navbar({},
+            navbarheader({},
+              navbarbrand({},
+                'logo'
+              ),
+              navbartoggle({})
+            ),
+            navbarcollapse({},
+              nav({pullRight: true},
+                navitem({},
+                  'Who We Are'
+                ),
+                navdropdown({title: 'Ministries'},
+                  menuitem({},
+                    'menuitem1'
+                  )
+                ),
+                navdropdown({title: 'Sermons'},
+                  menuitem({},
+                    'menuitem1'
+                  )
+                ),
+                navitem({},
+                  'Ice Cream Evangelism'
+                )
+              )
+            )
+          )
+        )
+
       )
     );
   }
 }
 
-export default function header() {
+export default function footer() {
   return React.createElement(Header);
 };
