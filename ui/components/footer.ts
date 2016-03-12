6bc94af0a1c8078baf06bea55b921a66433b972e
Brad Smith <bradmsmith@live.com>
2016-03-11 16:20:09 -0800

    Use bootstrap grid on footer

--- ui/components/footer.ts
+++ ui/components/footer.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {RaisedButton} from 'material-ui';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 const {a, div, h4, img, li, ul} = React.DOM;
 
@@ -7,62 +8,73 @@ function raisedButton(props, ...children) {
   return React.createElement(RaisedButton, props, ...children);
 }
 
+function grid(props, ...children) {
+  return React.createElement(Grid, props, ...children);
+}
+
+function row(props, ...children) {
+  return React.createElement(Row, props, ...children);
+}
+
+function col(props, ...children) {
+  return React.createElement(Col, props, ...children);
+}
+
 class Footer extends React.Component<void, void> {
   render() {
     return React.DOM.footer({style: {display: 'flex', flexWrap: 'wrap', margin: 20, paddingTop: 40, borderTop: '1px solid #EEE', padding: '40 0', lineHeight: '2em'}},
 
-      div({style: {width: '20%'}},
-        img({src: 'https://www.compasshb.com/CBC-HB-logo.png', alt: 'Compass Bible Church Huntington Beach, CompassHB', style: {height: 40}})
-      ),
+      grid({},
 
-      div({style: {width: '20%'}},
-        h4({}, 'Ministries'),
-        ul({},
-          li({}, a({href: '/kids'},  'Kids')),
-          li({}, a({href: '/youth'}, 'Youth')),
-          li({}, a({href: '/college'}, 'College')),
-          li({}, a({href: '/sundayschool'}, 'Adult Sunday School'))
-        )
-      ),
+        row({},
+          col({md: 2},
+            img({src: 'https://www.compasshb.com/CBC-HB-logo.png', alt: 'Compass Bible Church Huntington Beach, CompassHB', style: {height: 40}})
+          ),
+          col({md: 2},
+            h4({}, 'Ministries'),
+            ul({style: {listStyleType: 'none'}},
+              li({}, a({href: '/kids'},  'Kids')),
+              li({}, a({href: '/youth'}, 'Youth')),
+              li({}, a({href: '/college'}, 'College')),
+              li({}, a({href: '/sundayschool'}, 'Adult Sunday School'))
+            )
+          ),
+          col({md: 2},
+            h4({}, 'Resources'),
+            ul({style: {listStyleType: 'none'}},
+              li({}, a({href: '/read'}, 'Scripture of the Day')),
+              li({}, a({href: '/sermons'}, 'Sermons')),
+              li({}, a({href: '/songs'}, 'Worship')),
+              li({}, a({href: '/videos'}, 'Videos'))
+            )
+          ),
+          col({md: 2},
+            h4({}, 'Social'),
+            ul({style: {listStyleType: 'none'}},
+              li({}, a({href: 'https://www.facebook.com/compasshb'}, 'Facebook')),
+              li({}, a({href: 'https://instagram.com/compasshb'}, 'Instagram')),
+              li({}, a({href: 'https://twitter.com/compasshb'}, 'Twitter')),
+              li({}, a({href: 'https://vimeo.com/compasshb'}, 'Vimeo')),
+              li({}, a({href: 'https://appsto.re/us/n_WA6.i'}, 'iPhone App')),
+              li({}, a({href: 'https://play.google.com/store/apps/details?id=com.compasshb.mobile'}, 'Android App'))
+            )
+          ),
+          col({md: 4},
+            a({href: "/giving"},
+              raisedButton({label: "Give", style: {margin: 12, padding: '0 50px'}})
+            ),
 
-      div({style: {width: '20%'}},
-        h4({}, 'Resources'),
-        ul({},
-          li({}, a({href: '/read'}, 'Scripture of the Day')),
-          li({}, a({href: '/sermons'}, 'Sermons')),
-          li({}, a({href: '/songs'}, 'Worship')),
-          li({}, a({href: '/videos'}, 'Videos'))
-        )
-      ),
-
-      div({style: {width: '20%'}},
-        h4({}, 'Social'),
-        ul({},
-          li({}, a({href: 'https://www.facebook.com/compasshb'}, 'Facebook')),
-          li({}, a({href: 'https://instagram.com/compasshb'}, 'Instagram')),
-          li({}, a({href: 'https://twitter.com/compasshb'}, 'Twitter')),
-          li({}, a({href: 'https://vimeo.com/compasshb'}, 'Vimeo')),
-          li({}, a({href: 'https://appsto.re/us/n_WA6.i'}, 'iPhone App')),
-          li({}, a({href: 'https://play.google.com/store/apps/details?id=com.compasshb.mobile'}, 'Android App'))
-        )
-      ),
-
-      div({style: {width: '20%'}},
-        a({href: "/giving"},
-          raisedButton({label: "Give", style: {margin: 12, padding: '0 50px'}})
+            h4({}, 'Contact'),
+            ul({},
+              li({}, '5082 Argosy, Huntington Beach, CA 92649'),
+              li({}, '(714) 895-0034'),
+              li({}, 'info@compasshb.com')
+            )
+          )
         ),
 
-        h4({}, 'Contact'),
-        ul({},
-          li({}, '5082 Argosy, Huntington Beach, CA 92649'),
-          li({}, '(714) 895-0034'),
-          li({}, 'info@compasshb.com')
-        )
-      ),
-
-      div({style: {flex: '1 100%'}},
-        div({style: {padding: 10, textAlign: "right", backgroundColor: "#222222", color: "#A9A9A9"}},
-          'Copyright 2014-2016 Compass Bible Church Huntington Beach'
+        row({style: {padding: 10, textAlign: "right", backgroundColor: "#222222", color: "#A9A9A9"}},
+            'Copyright 2014-2016 Compass Bible Church Huntington Beach'
         )
       )
     );
