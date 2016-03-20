7e9e376f9748e10fef2638b4e61ba759b26b3173
Brad Smith <bradmsmith@live.com>
2016-03-19 21:46:08 -0700

    Copy content over to About Us / Who we are page

--- ui/pages/about/distinctives.tsx
+++ ui/pages/about/distinctives.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {Header} from "../../components/header";
 import {Footer} from "../../components/footer";
 import {SideNav} from "../../components/sideNav"
-
+    
 export class AboutDistinctivesPage extends React.Component<{}, {}> {
   render() {
     return (<div>
