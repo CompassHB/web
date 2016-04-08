59cfb7a6cdc52023ff1837a69956a34b5c482af2
Evan Winslow <evan.b.winslow@gmail.com>
2016-04-08 06:45:42 +0000

    Support Material Icons via the Google fonts API

--- server.ts
+++ server.ts
@@ -37,6 +37,7 @@ function renderFullHtmlPage(render: () => Promise<React.ReactElement<any>>): Pro
             <script src="/ui/pages/index-client.bundle.js" async></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
             <link href='https://fonts.googleapis.com/css?family=Roboto|Fira+Sans:700' rel='stylesheet' type='text/css'>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
           </head>
           <body>
