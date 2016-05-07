78127098d4d2e2ccdde7701aa984baa76f75ae4a
Evan Winslow <evan.b.winslow@gmail.com>
2016-05-07 23:25:37 +0000

    Consolidate custom .d.ts files into one
    This just reduces clutter of the top-level directory

--- react-tap-event-plugin.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "react-tap-event-plugin" {
-  function injectTapEventPlugin(): void;
-
-  module injectTapEventPlugin { }
-
-  export = injectTapEventPlugin;
-}
--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview Dumping ground for all the custom typings we need that aren't
+ *     already provided by the libraries themselves or by DefinitelyTyped.
+ */
+
 interface String {
   includes(substr: string): boolean;
 }
@@ -6,3 +11,29 @@ declare module 'node-fetch' {
     function fetch(url: string|Request, init?: RequestInit): Promise<Response>;
     export default fetch;
 }
+
+declare module "node-sass" {
+  interface RenderOptions {
+    file: string,
+    outputStyle?: string, // 'compressed'
+    outFile?: string,
+    sourceMap?: boolean, // or an absolute or relative-to-outFile path
+  }
+
+  interface RenderResult {
+    map: { toString(): string },
+    css: { toString(): string },
+    stats: Object,
+  }
+
+  export function renderSync(options: RenderOptions): RenderResult;
+}
+
+declare module "react-tap-event-plugin" {
+  function injectTapEventPlugin(): void;
+
+  module injectTapEventPlugin { }
+
+  export = injectTapEventPlugin;
+}
+
