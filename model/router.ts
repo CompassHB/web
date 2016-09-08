1ab9488575ad24c0075338b097aa867ecf1b046e
Evan Winslow <evan.b.winslow@gmail.com>
2016-09-08 16:00:16 +0000

    Upgrade to TypeScript 2.1 (dev), use async/await, and drop observables
    I find this to be eminently more understandable than the previous functional-style observables-based code.

--- model/router.ts
+++ model/router.ts
@@ -1,218 +1,241 @@
 import * as Router from "falcor-router";
 import * as moment from "moment";
 import fetch from 'node-fetch';
-import { Observable } from "rxjs";
-import { PathValue, ref } from "falcor-json-graph";
+import { PathValue, Range, error, ref } from "falcor-json-graph";
 
-function wpFetch(resource: string, options: { method?: 'head' } = {}) {
-  return fetch('https://api.compasshb.com/wp-json/wp/v2' + resource, options);
+class CompassHBWordpress {
+  constructor(private baseUrl: string) { }
+
+  async getJson(resource: string) {
+    const response: Response = await fetch(this.baseUrl + resource);
+    const json: any = await response.json();
+
+    if (response.status >= 400) {
+      throw new Error(json.message);
+    }
+
+    return json;
+  }
+
+  getMedia(id: number) {
+    return this.getJson(`/media/${id}`);
+  }
+
+  getSermons({offset = 0, limit = 100}): Promise<Array<{ slug: string }>> {
+    return this.getJson(`/posts?categories=1&offset=${offset}&per_page=${limit}`);
+  }
+
+  getUser(id: number): Promise<{ id: number, name: string, slug: string }> {
+    return this.getJson(`/users/${id}`);
+  }
+
+  head(resource: string) {
+    return fetch(this.baseUrl + resource, { method: 'head' });
+  }
 }
 
-function wpGetJson(resource: string) {
-  return wpFetch(resource).then((response) => {
-    return response.json().then((json) => {
-      if (response.status >= 400) {
-        throw new Error(json.message);
-      }
-
-      return json;
-    });
-  });
-}
-
-function wpGetMedia(id: number) {
-  return wpGetJson(`/media/${id}`);
-}
-
-function wpGetSermons({offset = 0, limit = 100}): Observable<{ slug: string }> {
-  return Observable.fromPromise(wpGetJson(`/posts?categories=1&offset=${offset}&per_page=${limit}`))
-    .concatMap((sermons: Array<{ slug: string }>) => Observable.from(sermons));
-}
-
-function wpGetUser(id: number): Promise<{ id: number, name: string, slug: string }> {
-  return wpGetJson(`/users/${id}`);
-}
+const wp = new CompassHBWordpress('https://api.compasshb.com/wp-json/wp/v2');
 
 
 export const router = new Router([
   {
     route: 'people.byId[{integers:ids}]["name","slug"]',
-    get(pathSets: any) {
-      const ids = <Observable<number>>Observable.from(pathSets.ids);
-      const props = <Observable<string>>Observable.from(pathSets[3]);
+    async get([, , ids, props]: [any, any, Array<number>, Array<string>]): Promise<Array<PathValue>> {
+      const results: Array<PathValue> = [];
 
-      return ids.concatMap((id) => {
-        return Observable.fromPromise(wpGetUser(id)).concatMap((person) => {
-          return props.map((prop) => {
-            const path = ['people', 'byId', id, prop];
+      for (const id of ids) {
+        const person = await wp.getUser(id);
 
+        for (const prop of props) {
+          const path = ['people', 'byId', id, prop];
+
+          try {
             switch (prop) {
               case 'name':
-                return { path, value: person.name };
+                results.push({ path, value: person.name });
+                break;
               case 'slug':
-                return { path, value: person.slug };
+                results.push({ path, value: person.slug });
+                break;
               default:
                 throw new Error('Unrecognized property: ' + prop);
             }
-          });
-        });
-      }).toArray().toPromise();
+          } catch (e) {
+            results.push({ path, value: error(e.message) });
+          }
+        }
+      }
+
+      return results;
     },
   },
   {
-    route: 'events.featured[{ranges:ranges}]',
-    get(pathSets: any) {
+    route: 'events.featured[{ranges}]',
+    async get([, , ranges]: any): Promise<Array<PathValue>> {
       // https://api.compasshb.com/wp-json/wp/v2/tribe_events?_embed&filter[tribe_events_cat]=Show on Homepage
-      return Promise.resolve([]);
+      return [];
     },
   },
   {
     route: 'events.featured.length',
-    get(pathSets: any) {
-      return Promise.resolve([]);
+    async get(pathSets: any): Promise<Array<PathValue>> {
+      return [];
     },
   },
   {
-    route: 'events.upcoming[{ranges:ranges}]',
-    get(pathSets: any) {
+    route: 'events.upcoming[{ranges}]',
+    async get([, , ranges]: any): Promise<Array<PathValue>> {
       // https://api.compasshb.com/wp-json/wp/v2/tribe_events?_embed
-      return Promise.resolve([]);
+      return [];
     },
   },
   {
     route: 'events.upcoming.length',
-    get(pathSets: any) {
-      return Promise.resolve([]);
+    async get(pathSets: any): Promise<Array<PathValue>> {
+      return [];
     },
   },
   {
-    route: 'events.bySlug[{keys:slugs}]["startTime","endTime","title","description","coverImage","slug"]',
-    get(pathSets: any) {
+    route: 'events.bySlug[{keys}]["startTime","endTime","title","description","coverImage","slug"]',
+    async get([, , slugs, props]: any): Promise<Array<PathValue>> {
       // https://api.compasshb.com/wp-json/wp/v2/tribe_events?_embed&slug={slug}
-      return Promise.resolve([]);
+      return [];
     }
   },
   {
     route: 'series.recent[{ranges:ranges}]',
-    get(pathSets: any) {
+    async get(pathSets: any): Promise<Array<PathValue>> {
       // Series are "tags" in WP: https://api.compasshb.com/wp-json/wp/v2/tags?embed
-      return Promise.resolve([]);
+      return [];
     },
   },
   {
     route: 'series.recent.length',
-    get() {
-      return Promise.resolve([
+    async get(): Promise<Array<PathValue>> {
+      return [
         { path: ['series', 'recent', 'length'], value: 0 },
-      ]);
+      ];
     },
   },
   {
-    route: 'series.bySlug[{keys:slugs}]["title","description","coverImage","slug"]',
-    get(pathSets: any) {
+    route: 'series.bySlug[{keys}]["title","description","coverImage","slug"]',
+    async get([, , slugs, props]: any): Promise<Array<PathValue>> {
       // Series are "tags" in WP: https://api.compasshb.com/wp-json/wp/v2/tags?embed&slug={slug}
-      return Promise.resolve([]);
+      return [];
     },
   },
   {
     route: 'sermons.recent[{ranges:ranges}]',
-    get(pathSets: any) {
-      const ranges = <Observable<{ from: number, to: number }>>Observable.from(pathSets.ranges);
-      const obs = ranges.concatMap((range) => {
-        return wpGetSermons({
-          offset: range.from,
-          limit: range.to - range.from + 1,
-        }).map(({slug}: { slug: string }, i: number) => {
-          return {
-            // TODO(ewinslow): Cache the sermon bodies to avoid more round trips
-            path: ['sermons', 'recent', i],
-            value: ref(['sermons', 'bySlug', slug]),
-          };
-        });
-      });
+    get([, , ranges]: [any, any, Array<Range>]): Promise<Array<PathValue>> {
+      const results: Array<PathValue> = [];
 
-      const promise = obs.toArray().toPromise();
+      // Not sure why, but TypeScript keeps using yield here when we try to make this an async function
+      // For now, we'll have to use this promise-chaining approach which is still nicer than Observables IMO
+      let promise = Promise.resolve(results);
+      for (const {from = 0, to} of ranges) {
+        promise = promise.then(() => wp.getSermons({ offset: from, limit: to - from + 1 })).then((sermons) => {
+          sermons.forEach(({slug}, i) => {
+            results.push({
+              path: ['sermons', 'recent', from + i],
+              value: ref(['sermons', 'bySlug', slug]),
+            });
+          });
+
+          return results;
+        });
+      }
 
       return promise;
     },
   },
   {
     route: 'sermons.recent.length',
-    get(pathSets: any) {
-      return wpFetch(`/posts?categories=1`, { method: 'head' }).then((response) => {
-        return response.headers.get('X-WP-Total');
-      }).then((total) => ([{
+    async get(pathSets: any): Promise<Array<PathValue>> {
+      const response: Response = await wp.head(`/posts?categories=1`);
+
+      return [{
         path: ['sermons', 'recent', 'length'],
-        value: total,
-      }]));
+        value: response.headers.get('X-WP-Total'),
+      }];
     },
   },
   {
-    route: 'sermons.bySlug[{keys:slugs}]["title","content","slug","teacher","text","date","coverImage"]',
-    get(pathSets: any) {
-      const props = Observable.from(pathSets[3]);
-      const slugs = Observable.from(pathSets.slugs);
+    route: 'sermons.bySlug[{keys}]["title","content","slug","teacher","text","date","coverImage"]',
+    async get([, , slugs, props]: [any, any, Array<string>, Array<string>]): Promise<Array<PathValue>> {
+      const results: Array<PathValue> = [];
 
-      return slugs.concatMap((slug: string) => {
-        return Observable.fromPromise(wpGetJson(`/posts?_embed&slug=${slug}`))
-          .concatMap(([sermon]: Array<any>) => {
-            return props.concatMap((prop: string) => {
-              const path = ['sermons', 'bySlug', slug, prop];
-              try {
-                switch (prop) {
-                  case 'slug':
-                    return Observable.of({ path, value: sermon.slug });
-                  case 'content':
-                    return Observable.of({ path, value: sermon.content.rendered });
-                  case 'coverImage':
-                    return Observable.of({ path, value: sermon._embedded['wp:featuredmedia'][0].source_url });
-                  case 'date':
-                    return Observable.of({ path, value: moment(sermon.date).format('dddd, MMMM D, YYYY') });
-                  case 'teacher':
-                    return Observable.of({ path, value: sermon._embedded.author[0].name });
-                  case 'text':
-                    return Observable.of({ path, value: sermon.acf.text });
-                  case 'title':
-                    return Observable.of({ path, value: sermon.title.rendered });
-                  default:
-                    throw new Error('Unrecognized property: ' + prop);
-                }
-              } catch (e) {
-                return Observable.of({ path, value: e.message });
-              }
-            });
-          });
-      }).toArray().toPromise();
+      for (const slug of slugs) {
+        const [sermon] = await wp.getJson(`/posts?_embed&slug=${slug}`);
+
+        for (const prop of props) {
+          const path = ['sermons', 'bySlug', slug, prop];
+
+          try {
+            switch (prop) {
+              case 'slug':
+                results.push({ path, value: sermon.slug });
+                break;
+              case 'content':
+                results.push({ path, value: sermon.content.rendered });
+                break;
+              case 'coverImage':
+                results.push({ path, value: sermon._embedded['wp:featuredmedia'][0].source_url });
+                break;
+              case 'date':
+                results.push({ path, value: moment(sermon.date).format('dddd, MMMM D, YYYY') });
+                break;
+              case 'teacher':
+                results.push({ path, value: sermon._embedded.author[0].name });
+                break;
+              case 'text':
+                results.push({ path, value: sermon.acf.text });
+                break;
+              case 'title':
+                results.push({ path, value: sermon.title.rendered });
+                break;
+              default:
+                throw new Error('Unrecognized property: ' + prop);
+            }
+          } catch (e) {
+            results.push({ path, value: error(e.message) });
+          }
+        }
+      }
+
+      return results;
     },
   },
   {
     route: 'pages.bySlug[{keys:slugs}]["title","content","slug"]',
-    get(pathSets: any) {
-      const props = Observable.from(pathSets[3]);
-      const slugs = Observable.from(pathSets.slugs);
+    async get([, , slugs, props]: [any, any, Array<string>, Array<string>]): Promise<Array<PathValue>> {
+      const results: Array<PathValue> = [];
 
-      return slugs.concatMap((slug: string) => {
-        return Observable.fromPromise(wpGetJson(`/pages?_embed&slug=${slug}`))
-          .concatMap(([page]: Array<any>) => {
-            return props.concatMap((prop: string) => {
-              const path = ['pages', 'bySlug', slug, prop];
-              try {
-                switch (prop) {
-                  case 'slug':
-                    return Observable.of({ path, value: page.slug });
-                  case 'content':
-                    return Observable.of({ path, value: page.content.rendered });
-                  case 'title':
-                    return Observable.of({ path, value: page.title.rendered });
-                  default:
-                    throw new Error('Unrecognized property: ' + prop);
-                }
-              } catch (e) {
-                return Observable.of({ path, value: e.message });
-              }
-            });
-          });
-      }).toArray().toPromise();
+      for (const slug of slugs) {
+        const [page] = await wp.getJson(`/pages?_embed&slug=${slug}`);
+
+        for (const prop of props) {
+          const path = ['pages', 'bySlug', slug, prop];
+          try {
+            switch (prop) {
+              case 'slug':
+                results.push({ path, value: page.slug });
+                break;
+              case 'content':
+                results.push({ path, value: page.content.rendered });
+                break;
+              case 'title':
+                results.push({ path, value: page.title.rendered });
+                break;
+              default:
+                throw new Error('Unrecognized property: ' + prop);
+            }
+          } catch (e) {
+            results.push({ path, value: error(e.message) });
+          }
+        }
+      }
+
+      return results;
     },
   },
 ]);
